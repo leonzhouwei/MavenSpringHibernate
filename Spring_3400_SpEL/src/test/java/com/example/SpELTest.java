63b32e420ab0a4f3f03f1370481fb6668c2ff13f
zhouwei <leonzhouwei@gmail.com>
2013-07-23 16:00:02 +0800

    add some explanations for SpEL demo codes

--- Spring_3400_SpEL/src/test/java/com/example/SpELTest.java
+++ Spring_3400_SpEL/src/test/java/com/example/SpELTest.java
@@ -1,19 +1,37 @@
 package com.example;
 
 import org.junit.Test;
+import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+import com.example.model.User;
 
 @ContextConfiguration("classpath:beans.xml")
 public class SpELTest extends AbstractJUnit4SpringContextTests {
 
-	@Test
+//	@Test
 	public void test01() {
 		ExpressionParser parser = new SpelExpressionParser();
 		Expression expression = parser.parseExpression("'Hello, world!'");
 		System.out.println((String) expression.getValue());
+	}
+	
+	@Test
+	public void test02() {
+		User user = new User();
+		user.setUsername("bob");
+		user.setPassword("bob");
+		
+		EvaluationContext ec = new StandardEvaluationContext(user);
+		
+		ExpressionParser parser = new SpelExpressionParser();
+		Expression expression = parser.parseExpression("username"); // i.e. user.getUsername
+		System.out.println((String)expression.getValue(ec));
+		System.out.println((String)expression.getValue(user));
 	}
 }
