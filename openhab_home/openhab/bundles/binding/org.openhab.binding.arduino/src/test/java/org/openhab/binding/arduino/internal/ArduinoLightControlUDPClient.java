442607cf58a99fd8b6249de9db945fd0117bd758
zhouwei <leonzhouwei@gmail.com>
2014-05-02 15:37:29 +0800

    refine the unit test classed

--- openhab_home/openhab/bundles/binding/org.openhab.binding.arduino/src/test/java/org/openhab/binding/arduino/internal/ArduinoLightControlUDPClient.java
+++ openhab_home/openhab/bundles/binding/org.openhab.binding.arduino/src/test/java/org/openhab/binding/arduino/internal/ArduinoLightControlUDPClient.java
@@ -7,7 +7,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public final class UDPClient {
+public final class ArduinoLightControlUDPClient {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
@@ -16,17 +16,11 @@ public final class UDPClient {
 		System.out.println("client socket established at " + clientSocket.getLocalSocketAddress());
 		InetAddress addr = InetAddress.getByName("192.168.1.177");
 		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
 		String sentence = inFromUser.readLine();
 		sendData = sentence.getBytes();
 		DatagramPacket sendPacket = new DatagramPacket(sendData,
 				sendData.length, addr, 8888);
 		clientSocket.send(sendPacket);
-//		DatagramPacket receivePacket = new DatagramPacket(receiveData,
-//				receiveData.length);
-//		clientSocket.receive(receivePacket);
-//		String modifiedSentence = new String(receivePacket.getData());
-//		System.out.println("FROM SERVER: " + modifiedSentence);
 		clientSocket.close();
 	}
 
